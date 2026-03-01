8114b72102f55bbf6d358d9304d0712c9753dc4d
a.isaeva <201250056+rennaiis@users.noreply.github.com>
2026-03-02 02:30:53 +0300

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -46,8 +46,30 @@ function getDayOfWeek(n: ReturnType<Date['getDay']>){
       break
   }
 }
+function getIcon(n: number): string{
+  if (n == 1000){
+    return '/icon-sunny.webp'
+  }else if (n==1003){
+    return '/icon-partly-cloudy.webp'
+  }else if (n==1006 || n==1009){
+    return '/icon-overcast.webp'
+  }else if ([1063, 1072, 1150, 1153, 1168, 1171].includes(n)){
+    return '/icon-drizzle.webp'
+  }else if ([1030, 1135, 1147].includes(n)){
+    return '/icon-fog.webp'
+  }else if ([1180, 1183, 1186, 1189, 1192, 1195, 1198, 1201, 1240, 1243, 1246].includes(n)){
+    return '/icon-rain.webp'
+  }else if ([1066, 1069, 1114, 1117, 1204, 1207, 1210, 1213, 1216, 1219, 1222, 1225, 1237, 1249, 1252, 1255, 1258, 1261, 1264].includes(n)){
+    return '/icon-snow.webp'
+  }else if ([1087, 1273, 1276, 1279, 1282].includes(n)){
+    return '/icon-storm.webp'
+  }else{
+    return'/icon-sunny.webp'
+  }
+  
+}
 function App() {
-  const [city, setCity] = useState('jxkljlajclsa')
+  const [city, setCity] = useState('Yaroslavl')
   const[date, setDate]=useState(new Date)
   const [weatherData, setWeatherData] = useState<IWeatherData | null>(null)
   const [error, setError] = useState('')
@@ -60,6 +82,7 @@ function App() {
       }
       const data = await res.json()
       setWeatherData(data);
+      console.log(data)
       setDate(new Date());
       setError('')
       } 
@@ -95,7 +118,7 @@ function App() {
               </div>
               <div className='flex flex-col items-center'>
                 <div className='flex justify-between items-center'>
-                  <img src="/icon-sunny.webp" alt="icon" className="h-20" />
+                  <img src={getIcon(weatherData.current.condition.code)} alt="icon" className="h-20" />
                   <p className="text-5xl text-indigo-50 font-semibold italic">{weatherData.current.temp_c}°C</p>
                 </div>
                 <p className='font-semibold text-lg'>{weatherData.current.condition.text}</p>
