7197931be8d7e6f7801e094b341fcf4ba0eaac43
a.isaeva <201250056+rennaiis@users.noreply.github.com>
2026-03-02 00:23:09 +0300

    display weather data in UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,63 @@
 import { useState, useEffect } from 'react'
 import './index.css' 
 const key = 'fec76df21593484e9a8142548260103'
+
+interface IWeatherData{
+  location: {
+    name: string;
+    country: string;
+    region: string;
+  };
+  current:{
+    temp_c: number;
+    temp_f: number;
+    is_day: 0|1, 
+    feelslike_c: number;
+    humidity: number;
+    wind_kph: number;
+    condition: {
+      text: string;
+      code: number
+    } 
+  }
+}
+function getDayOfWeek(n: ReturnType<Date['getDay']>){
+  switch(n){
+    case 0: 
+      return 'Sunday'
+      break
+    case 1: 
+      return 'Monday'
+      break
+    case 2: 
+      return 'Tuesday'
+      break
+    case 3: 
+      return 'Wednesday'
+      break
+    case 4: 
+      return 'Thusday'
+      break
+    case 5: 
+      return 'Friday'
+      break
+    case 6: 
+      return 'Saturday'
+      break
+  }
+}
 function App() {
-  const [city, setCity] = useState('')
+  const [city, setCity] = useState('Annecy')
   const[date, setDate]=useState(new Date)
-  const [weatherData, setWeatherData] = useState('')
+  const [weatherData, setWeatherData] = useState<IWeatherData | null>(null)
   useEffect(()=>{
     async function getData() {
-    const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=Paris`);
+    const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`);
     const data = await res.json()
     setWeatherData(data)
-    console.log(data);
+    setDate(new Date())
   } 
+  getData()
   }, [])
   
 
@@ -24,29 +70,36 @@ function App() {
           <input type="text" placeholder="Enter city name" className='p-1 pl-6 w-full rounded-sm outline-none focus:ring-1 focus:ring-white/60' />
         </div>
         <button className='bg-indigo-600 p-1 rounded-sm transition-all hover:bg-indigo-900'>Search</button>
-        <div className="p-3 flex flex-col items-center bg-[url('/bg-today-small.svg')] hover:brightness-75 bg-center bg-cover rounded-xl h-max-[250px] aspect-[1.2/1] min-[350px]:bg-[url('/bg-today-large.svg')] min-[350px]:aspect-3/1 min-[400px]:flex-row min-[400px]:justify-between min-[400px]:p-6">
-          <div>
-            <h2 className='text-lg font-bold '>London, Great Britain</h2>
-            <p className='text-md font-semibold'>Tuesday, 25.02.2026</p>
+        {weatherData && (
+          <div className="p-3 flex flex-col items-center bg-[url('/bg-today-small.svg')] hover:brightness-75 bg-center bg-cover rounded-xl h-max-[250px] aspect-[1.2/1] min-[350px]:bg-[url('/bg-today-large.svg')] min-[350px]:aspect-3/1 min-[450px]:flex-row min-[450px]:justify-between min-[450px]:p-6">
+            <div>
+              <h2 className='text-lg font-bold '>{weatherData.location.name}, {weatherData.location.region}, {weatherData.location.country}</h2>
+              <p className='text-md font-semibold'>{getDayOfWeek(date.getDay())}, {date.getDate()}.{date.getMonth()+1}.{date.getFullYear()}</p>
+            </div>
+            <div className='flex flex-col items-center'>
+              <div className='flex justify-between items-center'>
+                <img src="/icon-sunny.webp" alt="icon" className="h-20" />
+                <p className="text-5xl text-indigo-50 font-semibold italic">{weatherData.current.temp_c}°C</p>
+              </div>
+              <p className='font-semibold text-lg'>{weatherData.current.condition.text}</p>
+            </div>
           </div>
-          <div className='flex justify-between items-center'>
-            <img src="/icon-sunny.webp" alt="icon" className="h-20" />
-            <p className="text-5xl text-indigo-50 font-semibold italic">11°C</p>
-          </div>
-        </div>
+        )}
         <div className="flex gap-2 w-full">
-        <div className='bg-slate-600 p-5 rounded-2xl w-full transition-all hover:bg-slate-700'>
-          <p className=''>Humidity:</p>
-          <p className='text-2xl'>20%</p>
-        </div>
-        <div className='bg-slate-600 p-5 rounded-2xl w-full transition-all hover:bg-slate-700'>
-          <p className=''>Wind:</p>
-          <p className='text-2xl'>22 km/h</p>
+          <div className='bg-slate-600 p-5 rounded-2xl w-full transition-all hover:bg-slate-700'>
+            <p className=''>Humidity:</p>
+            <p className='text-2xl'>{weatherData?.current.humidity}%</p>
+          </div>
+          <div className='bg-slate-600 p-5 rounded-2xl w-full transition-all hover:bg-slate-700'>
+            <p className=''>Wind:</p>
+            <p className='text-2xl'>{weatherData?.current.wind_kph} km/h</p>
+          </div>
+          <div className='bg-slate-600 p-5 rounded-2xl w-full transition-all hover:bg-slate-700'>
+            <p className=''>Feels like:</p>
+            <p className='text-2xl'>{weatherData?.current.feelslike_c}°C</p>
+          </div>
         </div>
       </div>
-      </div>
-      
-      
     </div>
   );
 }
