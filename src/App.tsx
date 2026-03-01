6b1b9e1fe68d5e7538f046592d51dd051a5e9348
a.isaeva <201250056+rennaiis@users.noreply.github.com>
2026-03-01 22:26:12 +0300

    all the styles are added and adopted for modiles

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,32 @@ function App() {
       <div className='flex flex-col gap-[5px] w-full max-w-[500px]'>
         <div className='bg-slate-600 rounded-sm relative'>
         <img src="icon-search.svg" className='absolute opacity-60 w-5 h-5 pt-1 pl-1'/>
-          <input type="text" placeholder="Enter city name" className='p-1 pl-6 w-full  rounded-sm outline-none focus:ring-1 focus:ring-white/60' />
+          <input type="text" placeholder="Enter city name" className='p-1 pl-6 w-full rounded-sm outline-none focus:ring-1 focus:ring-white/60' />
         </div>
         <button className='bg-indigo-600 p-1 rounded-sm hover:bg-indigo-900'>Search</button>
-      </div>
-        <div className="weather-card">
-          <h2>London, Great Britain</h2>
-          <img src="" alt="icon" className="weather-icon" />
-          <p className="temperature">11°C</p>
-          <p className="condition">rainy</p>
-          <div className="weather-details">
-            <p>Humidity: 20%</p>
-            <p>Wind: 22 km/h</p>
+        <div className="p-3 flex flex-col items-center bg-[url('/bg-today-small.svg')] bg-center bg-cover rounded-xl h-max-[250px] aspect-[1.2/1] min-[350px]:bg-[url('/bg-today-large.svg')] min-[350px]:aspect-3/1 min-[400px]:flex-row min-[400px]:justify-between min-[400px]:p-6">
+          <div>
+            <h2 className='text-lg font-bold '>London, Great Britain</h2>
+            <p className='text-md font-semibold'>Tuesday, 25.02.2026</p>
+          </div>
+          <div className='flex justify-between items-center'>
+            <img src="/icon-sunny.webp" alt="icon" className="h-20" />
+            <p className="text-5xl text-indigo-50 font-semibold italic">11°C</p>
           </div>
         </div>
+        <div className="flex gap-2 w-full">
+        <div className='bg-slate-600 p-5 rounded-2xl w-full'>
+          <p className=''>Humidity:</p>
+          <p className='text-2xl'>20%</p>
+        </div>
+        <div className='bg-slate-600 p-5 rounded-2xl w-full'>
+          <p className=''>Wind:</p>
+          <p className='text-2xl'>22 km/h</p>
+        </div>
+      </div>
+      </div>
+      
+      
     </div>
   );
 }
